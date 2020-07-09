2af4db244c3dcfba4ccf51e69cce96365c2a99f0
Sai Hemanth <hemanth.six4u@gmail.com>
2020-07-09 16:39:38 -0500

    new we app by hemanth

---
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,35 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class SpeakerServiceImpl implements SpeakerService {
+
+    private SpeakerRepository repository ;
+
+    public SpeakerService(){
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
+    public SpeakerServiceImpl (SpeakerRepository speakerRepository){
+        System.out.println("SpeakerServiceImpl repository constructor");
+        repository = speakerRepository;
+
+    }
+    public List<Speaker> findAll(){
+        return repository.findAll();
+
+    }
+
+    @Autowired
+    public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter ");
+        this.repository = repository;
+    }
+
+
+}
